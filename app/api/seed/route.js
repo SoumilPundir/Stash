aceb2ffcc60281f067a42a766dfedb6088c0cc9b
Soumil Pundir <soumil99pundir@gmail.com>
2025-11-15 15:26:29 +0530

    Deploy

--- app/api/seed/route.js
+++
@@ -1,8 +0,0 @@
-export const dynamic = "force-dynamic";
-import { seedTransactions } from "@/actions/seed";
-
-export async function GET() {
-  const result = await seedTransactions();
-  return Response.json(result);
-}
-
